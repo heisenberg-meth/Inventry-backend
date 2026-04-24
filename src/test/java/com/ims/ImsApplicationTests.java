330404e5f3a0ecd06a91c3b9fe86649686d454a6
heisenberg-meth <nmdevi63@gmail.com>
2026-04-24 13:34:08 +0530

    test: use unique identifiers for tenants in AuthIntegrationTest and clear log file

--- src/test/java/com/ims/ImsApplicationTests.java
+++ src/test/java/com/ims/ImsApplicationTests.java
@@ -3,10 +3,23 @@ package com.ims;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-@SpringBootTest
-@ActiveProfiles("test")
-class ImsApplicationTests extends BaseIntegrationTest {
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.NONE,
+  properties = {
+    "spring.flyway.enabled=false",
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=none"
+  }
+)
+@ActiveProfiles("test")
+class ImsApplicationTests {
+  @MockitoBean
+  private RedisTemplate<String, Object> redisTemplate;
   @Test
   void contextLoads() {}
 }
